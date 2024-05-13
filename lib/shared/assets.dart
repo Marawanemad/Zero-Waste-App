add6318f92660ae9c75da542415b3ae24e0134b4
Serag Amged <seragamged2002@gmail.com>
2024-05-13 06:46:47 +0300

    assets colors modules

---
+++ lib/shared/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesOnboardingOnboarding1
+  /// assets/images/onboarding/onboarding1.svg
+  static const String imagesOnboardingOnboarding1 = "assets/images/onboarding/onboarding1.svg";
+
+  /// Assets for imagesOnboardingOnboarding2
+  /// assets/images/onboarding/onboarding2.svg
+  static const String imagesOnboardingOnboarding2 = "assets/images/onboarding/onboarding2.svg";
+
+  /// Assets for imagesOnboardingOnboarding3
+  /// assets/images/onboarding/onboarding3.svg
+  static const String imagesOnboardingOnboarding3 = "assets/images/onboarding/onboarding3.svg";
+}
+
