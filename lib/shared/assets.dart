373cc449cf96bb89f6bab52bc2fc40a3c7a43969
Marwanemad <marawanemad717@gmail.com>
2024-05-17 03:03:04 +0300

    authentication screen

--- lib/shared/assets.dart
+++ lib/shared/assets.dart
@@ -1,20 +1,27 @@
 class Assets {
   Assets._();
-  
+
   /// Assets for iconsArrow
   /// assets/icons/arrow.svg
   static const String iconsArrow = "assets/icons/arrow.svg";
 
   /// Assets for imagesOnboardingOnboarding1
   /// assets/images/onboarding/onboarding1.png
-  static const String imagesOnboardingOnboarding1 = "assets/images/onboarding/onboarding1.png";
+  static const String imagesOnboardingOnboarding1 =
+      "assets/images/onboarding/onboarding1.png";
 
   /// Assets for imagesOnboardingOnboarding2
   /// assets/images/onboarding/onboarding2.png
-  static const String imagesOnboardingOnboarding2 = "assets/images/onboarding/onboarding2.png";
+  static const String imagesOnboardingOnboarding2 =
+      "assets/images/onboarding/onboarding2.png";
 
   /// Assets for imagesOnboardingOnboarding3
   /// assets/images/onboarding/onboarding3.png
-  static const String imagesOnboardingOnboarding3 = "assets/images/onboarding/onboarding3.png";
-}
+  static const String imagesOnboardingOnboarding3 =
+      "assets/images/onboarding/onboarding3.png";
 
+  /// Assets for imagesimagesAuthentication1
+  /// assets/images/authentication/auth_screen.png
+  static const String imagesAuthentication1 =
+      "assets/images/authentication/auth_screen.png";
+}
